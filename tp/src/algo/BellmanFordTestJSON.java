4ca0ad3b89ab647ac231a85abc513d64b880350e
Zelmine <zelmine31@gmail.com>
2025-03-14 17:01:31 +0100

    Lecture fichier json

--- tp/src/algo/BellmanFordTestJSON.java
+++ tp/src/algo/BellmanFordTestJSON.java
@@ -1,5 +1,19 @@
 package algo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 
 public class BellmanFordTestJSON {
+	    public static void main(String[] args) {
+	        ObjectMapper objectMapper = new ObjectMapper();
 
+	        try {
+	            File fichier = new File("test_cases.json");
+	            JeuDeTestEtOracleFormat fic = objectMapper.readValue(fichier, JeuDeTestEtOracleFormat.class);
+	            
+	            System.out.println(fic);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
